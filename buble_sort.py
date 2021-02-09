fbfaa3d6b0316a0aec33b9d88ff7680533efee8e
ronald1402 <ronaldjosuah@gmail.com>
2021-02-09 14:19:54 +0700

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -1,13 +1,15 @@
 def bubleSort(A):
     length = len(A);
-    
+    flag = 0
     for i in range(1,length):
         for j in range(0, length-i):
             if A[j] > A[j+1]:
                 temp = A[j]
                 A[j] = A[j+1]
                 A[j+1] = temp
-    
+                flag = 1
+        if flag == 0:
+            break
     return A
     
 res = bubleSort([3,2,5,1])
